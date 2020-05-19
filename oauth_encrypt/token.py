7cb35a0d9947762c59234024d3e80d05c82e6b26
Martin Morset <mamor@dfds.com>
2020-05-19 15:34:21 +0200

    finished reusable encrypted flow

--- oauth_encrypt/token.py
+++ oauth_encrypt/token.py
@@ -1,27 +1,75 @@
 from os import environ
+from pathlib import Path
+import json
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import TokenExpiredError
+from cryptography.fernet import Fernet
 
-class EncryptedToken:
-    def __init__(self):
+
+class EncryptedOauth2Client:
+    def __init__(self, config):
         # Read the reqired env variables
-        self._set_config_variables()
+        self._set_config_variables(config)
 
-        self._access_token = None
-        pass
+        # Set up the oauth2.0 session
+        _client = BackendApplicationClient(client_id=self._client_id)
+        self._session = OAuth2Session(client=_client)
 
-    def _set_config_variables(self):
-        self._token_url = environ["TOKEN_URL"]
-        self._client_id = environ["CLIENT_ID"]
-        self._client_secret = environ["CLIENT_SECRET"]
-        self._scope = environ["SCOPE"]
+        # Read the credentials if they exist, otherwise get new token
+        if Path("access_key.credential").exists():
+            self._read_token()
+        else:
+            self._get_fresh_token()
+
+    def _set_config_variables(self, config):
+        """This unpacks the config object to class attributes"""
+        self._token_url = config["token_url"]
+        self._client_id = config["client_id"]
+        self._client_secret = config["client_secret"]
+        self._scope = config["scope"]
+        self._encryptionkey = Fernet(config["encryptionkey"])
 
     def _get_fresh_token(self):
         """Get a new oauth token"""
-        pass
+        self._access_token = self._session.fetch_token(
+            token_url=self._token_url,
+            client_id=self._client_id,
+            client_secret=self._client_secret,
+        )
+        self._save_token()
 
-    @staticmethod
-    def _valid():
-        """Check that the current token is valid"""
-        return True
+    def _save_token(self):
+        encrypted_token = self._encryptionkey.encrypt(
+            json.dumps(self._access_token).encode("UTF-8")
+        )
+        # write the encrypted file
+        with open(Path("access_key.credential"), "wb") as file:
+            file.write(encrypted_token)
+
+    def _read_token(self):
+        with open(Path("access_key.credential"), "rb") as file:
+            # read all file data
+            file_data = file.read()
+        self._access_token = json.loads(
+            self._encryptionkey.decrypt(file_data).decode("UTF-8")
+        )
+
+    def get(self, url, params):
+        """Send a get-request to a url"""
+        try:
+            client = OAuth2Session(self._client_id, token=self._access_token)
+            r = client.get(url, params=params)
+        except TokenExpiredError:
+            self._get_fresh_token()
+            client = OAuth2Session(self._client_id, token=self._access_token)
+            r = client.get(url, params=params)
+        
+        # Raise errors if any
+        r.raise_for_status()
+
+        # Return payload as dictionary
+        return r.json()
 
     @property
     def access_token(self):
--- test_token.py
+++ test_token.py
@@ -1,8 +1,29 @@
-from oauth_encrypt.token import EncryptedToken
+from oauth_encrypt.token import EncryptedOauth2Client
+from os import environ
+import yaml
+
+config = {
+    "token_url": environ["TOKEN_URL"],
+    "client_id": environ["CLIENT_ID"],
+    "client_secret": environ["CLIENT_SECRET"],
+    "scope": environ["SCOPE"],
+    "encryptionkey": environ["ENCRYPTION_KEY"],
+}
+
+client = EncryptedOauth2Client(config)
 
 
 def main():
-    token = EncryptedToken().access_token
+    """Example using Encrypted Reusable Oauth Client"""
+    # Read YAML file with arguments
+    with open("args.yaml", "r") as stream:
+        args = yaml.safe_load(stream)
+
+    r = client.get(
+        "https://apitest.dfds.com/server/ddp/pbb/views/getstowedunits", params=args
+    )
+
+    print(f"Retrieved {len(r['elements'])} entries")
 
 
 if __name__ == "__main__":
